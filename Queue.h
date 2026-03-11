0812f3fff4cb0f064c5c7735c5d32f779cae2e6a
phavitaMUIC <phavita.cha@student.mahidol.edu>
2026-03-11 04:40:00 +0000

    a

--- Queue.h
+++ Queue.h
@@ -15,22 +15,44 @@ public:
 
 void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
-if(new_node){ 
+if(new_node!=NULL){ 
     /* Add head and tail for me please */
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size */
-	
+   //1. connect & Change tail
+   if(size>0){
+    tailPtr->set_next(new_node);
+    tailPtr = new_node;
+   }
+   else{
+    tailPtr = new_node;
+    headPtr = new_node;
+    
+
+   }
+   size++;
+  //2. (may be) change head  when the queue is empty
+  
+  //3. increase size 
+
  }
+ else 
+    cout<<"Out of memory"<<endl;
 }
 
 int Queue::dequeue(){
   if(size>0){
-    
+    if(headPtr!=NULL){
+    NodePtr t=headPtr;
+    int value= t->get_value();
+    headPtr=headPtr->get_next();
+    if(headPtr==NULL) tailPtr = NULL;
      /* Add head and tail for me please */
-          
+     --size;
+    delete t;  
+    return value;
     
   }
+}
+else
   cout<<"Empty queue";
   return -1;
 }
@@ -38,10 +60,16 @@ int Queue::dequeue(){
 
 Queue::Queue(){
     //initialize Queue
+    size=0;
+    headPtr=NULL;
+    tailPtr=NULL;
     
 }
 Queue::~Queue(){
-      
+      cout<<"Dequeue all"<<endl;
+      int n=size;
+      for(int i=0;i<n;i++)
+      dequeue();
     //delete all remaning Queue (i.e. DQ all) 
     
 }
